3401e434491a3831c7330a4b46c42bb6adad4787
shiyong <1549242163@qq.com>
2020-12-27 14:58:32 +0800

    T20201126 #comment 添加less文件

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './app.less'; 
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
